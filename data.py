b45eafa62b334ab7c98984831643834a0fae6bb1
Your Name <kent.thomas@Kents-MacBook-Air.local>
2020-12-15 22:55:43 -0500

    ran data.py and put the csv into html, and loaded it onto data.html

--- data.py
+++ data.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 johns_hopkins_df = pd.read_csv("resources/3_cases_and_deaths_by_state_timeseries.csv")
 
-johns_hopkins_df.to_html("data.html" index = False)
+johns_hopkins_df.to_html("data.html", index = False)
 
 johns_hopkins_table = johns_hopkins_df.to_html()
 
